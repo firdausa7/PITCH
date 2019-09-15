0d0d5d6d4be09834d35141605dc262f1563e4c74
firdausa7 <firdausa.salat@gmail.com>
2019-09-15 21:06:54 +0300

    import migrate and db initialization

--- config.py
+++ config.py
@@ -1,28 +1,3 @@
 class Config:
-    '''
-    General configuration parent class
-    '''
-    pass
-
-
-
-class ProdConfig(Config):
-    '''
-    Production  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-    pass
-
-
-class DevConfig(Config):
-    '''
-    Development  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-
     DEBUG = True
     SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://firdausa:12345@localhost:5432/pitch'
